14189056a02458e453120cba3d9f18d1ef6c011d
si9ma <hellob374@gmail.com>
2019-05-06 17:23:30 +0800

    add status code

--- judge/status.go
+++ judge/status.go
@@ -1,15 +1,45 @@
 package judge
 
-type Status string
+type Status struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
 
-const (
-	RuntimeErrorStatus = Status("RuntimeError")
-	CompileErrorStatus = Status("CompileError")
-	JudingStatus       = Status("Juding")
-	AcceptedStatus     = Status("Accepted")
-	RunTimeOutStatus   = Status("RunTimeOut")
-	OOMStatus          = Status("OOM")
-	WrongAnswerStatus  = Status("WrongAnswer")
-	SystemErrorStatus  = Status("SystemError")
-	NullStatus         = Status("")
+var (
+	AcceptedStatus = Status{
+		Code: 0,
+		Msg:  "Accepted",
+	}
+	JudingStatus = Status{
+		Code: 1,
+		Msg:  "Juding",
+	}
+	RuntimeErrorStatus = Status{
+		Code: 2,
+		Msg:  "RuntimeError",
+	}
+	CompileErrorStatus = Status{
+		Code: 3,
+		Msg:  "CompileError",
+	}
+	RunTimeOutStatus = Status{
+		Code: 4,
+		Msg:  "RunTimeOut",
+	}
+	OOMStatus = Status{
+		Code: 5,
+		Msg:  "OOM",
+	}
+	WrongAnswerStatus = Status{
+		Code: 6,
+		Msg:  "WrongAnswer",
+	}
+	SystemErrorStatus = Status{
+		Code: 7,
+		Msg:  "SystemError",
+	}
+	NullStatus = Status{
+		Code: -1,
+		Msg:  "",
+	}
 )
