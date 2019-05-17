60bcef2d3c8322f122b78cc1106130da5fb5fce8
si9ma <hellob374@gmail.com>
2019-05-17 12:07:23 +0800

    update model

--- model/user.go
+++ model/user.go
@@ -10,6 +10,7 @@ type User struct {
 	GithubUserID string    `gorm:"column:github_user_id" json:"github_user_id,omitempty"`
 	GithubName   string    `gorm:"column:github_name" json:"github_name"`
 	Email        string    `gorm:"column:email" json:"email" binding:"required,email,max=50"`
+	Name         string    `gorm:"column:name" json:"name" binding:"required,max=50,excludesall=!@#?"`
 	CreatedAt    time.Time `gorm:"column:created_at" json:"-"`
 	UpdatedAt    time.Time `gorm:"column:updated_at" json:"-"`
 	Role         int       `gorm:"column:role" json:"-"`
@@ -18,7 +19,6 @@ type User struct {
 	// so set omitempty && set this field to nil before return
 	Password         string `gorm:"-" json:"password,omitempty" binding:"omitempty,min=6,max=30"`
 	EncryptedPasswd  string `gorm:"column:passwd" json:"-"` // encrypted password in db
-	NickName         string `gorm:"column:nick_name" json:"nick_name" binding:"required,max=50,excludesall=!@#?"`
 	Signature        string `gorm:"column:signature" json:"signature" binding:"max=100"`
 	NoInOrganization string `gorm:"column:no_in_organization" json:"no_in_organization" binding:"max=30"`
 	Organization     string `gorm:"column:organization" json:"organization" binding:"max=50"`
