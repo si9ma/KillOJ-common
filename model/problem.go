699dfa3f2a3b26a1cb69ddc2874ac355121652f5
si9ma <hellob374@gmail.com>
2019-05-31 19:39:55 +0800

    disable grom auto create and update && but problem model need

--- model/problem.go
+++ model/problem.go
@@ -21,14 +21,14 @@ type Problem struct {
 	BelongType       BelongType        `gorm:"column:belong_type" json:"belong_type" binding:"exists,oneof=0 1 2"`
 	BelongToID       int               `gorm:"column:belong_to_id" json:"belong_to_id"`
 	CatalogID        int               `gorm:"column:catalog_id" json:"catalog_id" binding:"required"`
-	Catalog          Catalog           `json:"catalog" binding:"-"`
-	Tags             []Tag             `gorm:"many2many:problem_has_tag;association_autoupdate:true;association_autocreate:true" json:"tags" binding:"dive"`
-	ProblemSamples   []ProblemSample   `json:"samples" gorm:"association_autoupdate:true;association_autocreate:true" binding:"dive"`
-	ProblemTestCases []ProblemTestCase `json:"test_cases,omitempty" gorm:"association_autoupdate:true;association_autocreate:true" binding:"required,dive"`
-	UpVoteUsers      []UserWithOnlyID  `gorm:"many2many:user_vote_problem;association_jointable_foreignkey:user_id;" json:"up_vote_users" binding:"-"`
-	DownVoteUsers    []UserWithOnlyID  `gorm:"many2many:user_vote_problem;association_jointable_foreignkey:user_id;" json:"down_vote_users" binding:"-"`
-	Comments         []Comment         `json:"comments" binding:"-"`
-	Owner            User              `json:"owner" gorm:"foreignkey:OwnerID;" binding:"-"`
+	Catalog          Catalog           `json:"catalog" gorm:"association_autoupdate:false;association_autocreate:false" binding:"-"`
+	Tags             []Tag             `gorm:"many2many:problem_has_tag;" json:"tags" binding:"dive"`
+	ProblemSamples   []ProblemSample   `json:"samples" binding:"dive"`
+	ProblemTestCases []ProblemTestCase `json:"test_cases,omitempty" binding:"required,dive"`
+	UpVoteUsers      []UserWithOnlyID  `gorm:"many2many:user_vote_problem;association_jointable_foreignkey:user_id;association_autoupdate:false;association_autocreate:false" json:"up_vote_users" binding:"-"`
+	DownVoteUsers    []UserWithOnlyID  `gorm:"many2many:user_vote_problem;association_jointable_foreignkey:user_id;association_autoupdate:false;association_autocreate:false" json:"down_vote_users" binding:"-"`
+	Comments         []Comment         `json:"comments" gorm:"association_autoupdate:false;association_autocreate:false" binding:"-"`
+	Owner            User              `json:"owner" gorm:"foreignkey:OwnerID;association_autoupdate:false;association_autocreate:false" binding:"-"`
 	Limit            JSON              `gorm:"column:limit" json:"limit" binding:"required"` // todo store json in db may unreasonable
 }
 
